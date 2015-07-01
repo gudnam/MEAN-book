ed07a25acf6aee540b9bbfa4eaccf1120a7cbc61
ScottMoon <jiwonsis@naver.com>
2015-07-01 09:18:09 +0900

    add chapter 08 ex_files

--- ch08/README.md
+++ ch08/README.md
@@ -238,3 +238,60 @@ var success = !socket.write(data);
 }
 ```
 > //참고// socket_client.js :: 기본 TCP 소켓 클라이언트 구현
+
+### TCP 소켓 서버 구현
+가장 기본적인 수준에서 TCP 서버와 클라이언트 구현은 다음과 같다.
+- Server 객체 생성
+- 들어온 연결 처리
+- 데이터 송/수신
+- 추가적으로, 소켓 서버는 Server객체의 close와 error 이벤트와 클라이언트 Socket 연결 이벤트 처리
+
+> Server 객체를 사용해 소켓 서버를 구현하는 단계를 다룬다.
+```sh
+// net.createSever()함수를 사용해 소켓 서버 생성
+// 서버가 생성되면 server 객체는 연결 콜백 핸들러를 제공한다.
+var server = net.createSever(function(client) {
+    //Impolement the connection callback handle code here
+    //
+    //connection 이벤트 호출 내에서는 연결 속성과 란련된 설정을 할 수 있다.
+    // 소켓 연결 수신 종료시간 지정
+    client.setTimeout(500);
+    // 소켓 연결 인코딩 설정
+    client.setEncoding('utf8');
+    //
+    // 클라이언트 연결 시 발생하는 data, end, error, timeout, close  이벤트 또한 추가  시킬 수 있다.
+    client.on('data', function(data) {
+        console.log("Received from client: ", + data.toString());
+        //process  the data
+    });
+});
+// listen() 호출을 통해 포트 수신을 시작한다.
+server.listen(8107, function() {
+    //Impolement the listen callback handle code here
+    //
+    //서버 객체에 close와 error이벤트를 지원할 수 있다.
+    server.on('close', function() {
+        console.log('Server Terminated');
+    });
+    server.on('error', function(err) {
+    });
+});
+//
+//서버 내에서 데이터를 쓰려면 코드 내에 write()로 명령을 구현한다.
+//만약 클라이언트에서 많은 데이터를 쓰는 경우 drain 이벤트 핸들러를 구현하여 버퍼가 빈 경우에 다시 쓰기 작업을 수행할 수 있도록 한다.
+//drain 이벤트를 처리할 경우, 버퍼가 가득 차서 write()의 명령이 실패가 발생한 경우에 소켓에 쓰기 조절이 필요한 경우에는 도움이 된다.
+function writeData(socket, data) {
+    // 데이터의 버퍼가 빈경우를 체크해 줄 수있게 생성한다.
+    var success = !socket.write(data);
+    // !success란 쓰기가 가능할 경우이다.
+    if (!success) {
+        (function (socket, data) {
+            socket.once('drain', function() {
+                writeData(socket, data);
+            });
+        }) (socket, data);
+    }
+}
+```
+> //참고// socket_server.js :: 기본 TCP 소켓 서버 구현 (테스트 하실 경우에는 socket_server.js 실행하시고, socket_client.js를 실행하면 서로간의 주고 받는 데이터가 콘솔로 보일 것이다.)
+
--- ch08/socket_client.js
+++ ch08/socket_client.js
@@ -2,33 +2,50 @@
  * Created by scottmoon on 6/30/15.
  */
 
+
+/*
+ 여기서는 서버를 예시로 생성하여 소켓을 넣었다.
+ 한 서버에 소캣3개를 생성하여 각각의 소캣이름과 랜덤포트 번호를 부여받아 생성하였다.
+ 각각의 소캣의 열려있는 상태로 서버와 동시에 통신하고 있다.
+ */
 var net = require('net');
 
-// net.createServer()으로 소켓 서버를 생성한다. (예제 테스트때문에 임시로 생성.)
-var server = net.createServer(function (socket) {
-    socket.write('Echo server\r\n');
-    socket.pipe(socket);
-});
 
-// 서버의 리스닝 포트와 호스트는 클라이언트의 호스티와 동일해야한다.
-server.listen(8107, 'localhost');
+var options = {
+    port: 8107,
+    host: 'localhost'
+};
+
+//// net.createServer()으로 소켓 서버를 생성한다. (예제 테스트때문에 임시로 생성.)
+//var server = net.createServer(function (client) {
+//    //각각의 클라이언트를 server와 연결해준다.
+//    client.pipe(client)
+//});
+//
+//// 8107포트로 연결 수신
+//server.listen(8107, function () {
+//    console.log('Server listening for connections');
+//});
 
 function getConnection(connName) {
-    var client = net.connect({port: 8107, host: 'localhost'}, function () {
+    // options는 서버와 연결할 포트를 지정하는곳이다.
+    // 예제에서는 한서버의 옵션을 두기때문에 options를 전역으로 두었다.
+    var client = net.connect(options, function () {
         console.log(connName + ' Connected: ');
-        console.log(' local = %s:%s', this.localAddress, this.remotePort);
+        console.log('   local = %s:%s', this.localAddress, this.localPort);
+        console.log('   remote = %s:%s', client.remoteAddress, client.remotePort);
         this.setTimeout(500);
         this.setEncoding('utf8');
 
         this.on('data', function (data) {
-            console.log(connName + " From Server: " + data.toString());
+            console.log(connName + " From Server: Sending: " + data.toString());
             this.end();
         });
         this.on('end', function () {
             console.log(connName + ' Client disconnected');
         });
-        this.on('error', function () {
-            console.log('Socket Error: ' + JSON.stringify(err));
+        this.on('error', function (err) {
+            console.log('Socket Error: ', JSON.stringify(err));
         });
         this.on('timeout', function () {
             console.log('Socket Timed Out');
@@ -50,6 +67,7 @@ function writeData(socket, data) {
         })(socket, data);
     }
 }
+
 
 var Dwarves = getConnection("Dwarves");
 var Elves = getConnection("Elves");
